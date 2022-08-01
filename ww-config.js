2ee8be4b5595a50859204048dd36d886d7d2dfde
Dams-d <damien.duro8@gmail.com>
2022-08-01 17:49:47 +0200

    Update config

--- ww-config.js
+++ ww-config.js
@@ -18,8 +18,8 @@ export default {
             'dataType',
             ['labels', 'datasets'],
             'data',
+            'dataError',
             [
-                'dataError',
                 'xAxisTitle',
                 'dataXField',
                 'dataXFieldProperty',
